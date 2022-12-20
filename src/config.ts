ecd21f0b3b2419d7ae7212cf55427422db4ef3c0
WesleyOMorrow <xmy9602@gmail.com>
2022-12-20 14:53:08 +0100

    update index page

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Xiang Li';
-export const SITE_DESCRIPTION = "Xiang Li's personal website!";
+export const SITE_DESCRIPTION = "HODL and BUIDL";
 //export const SITE_IMAGE = '/adriangao.jpg'
 export const SITE_FAVICON = '/favicon.png';
 export const CV_PATH = '/cv.pdf'
